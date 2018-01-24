a6228ae6d6d9cf1d7c7eeacba026f81bf561d6f8
David Johnson <djohnso08@yahoo.com>
2018-01-24 14:10:37 -0800

    added function to pike store

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@
 
 var hoursArray = [' 6am: ',' 7am: ',' 8am: ',' 9am: ',' 10am: ',' 11am: ',' 12pm: ',' 1pm: ',' 2pm: ',' 3pm: ',' 4pm: ',' 5pm: ',' 6pm: ',' 7pm: ',' 8pm: '];
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-//   },
-
 var pike = {
   store: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCust: 6.3,
-  randNum: 0,
+  randNum: function() {
+    return Math.floor(Math.random() * (pike.maxCust - pike.minCust)) + 1;
+  },
   cookiesPerHr: [],
   results: []
 };
