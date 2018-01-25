02f5a8ae986a51c4bf83dbaca053cd7f32f37495
David Johnson <djohnso08@yahoo.com>
2018-01-24 18:27:58 -0800

    added random number function to all objects

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var hoursArray = [' 6am: ',' 7am: ',' 8am: ',' 9am: ',' 10am: ',' 11am: ',' 12pm: ',' 1pm: ',' 2pm: ',' 3pm: ',' 4pm: ',' 5pm: ',' 6pm: ',' 7pm: ',' 8pm: '];
 
-var pike = {
+var Pike = {
   store: '1st and Pike',
   minCust: 23,
   maxCust: 65,
@@ -10,51 +10,49 @@ var pike = {
   randNum: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + 1;
   },
-  cookiesPerHr: '' ,
-  totalCookies: [],
-};
-
-var pike = {
-  store: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCust: 6.3,
-  randNum: 0,
   cookiesPerHr: [],
 };
 
-var seaTac = {
+var SeaTac = {
   store: 'SeaTac Airport',
   minCust: 3,
   maxCust: 24,
   avgCust: 1.2,
-  randNum: 0,
+  randNum: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + 1;
+  },
   cookiesPerHr: [],
 };
 
-var seattle = {
+var Seattle = {
   store: 'Seattle Center',
   minCust: 11,
   maxCust: 38,
   avgCust: 3.7,
-  randNum: 0,
+  randNum: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + 1;
+  },
   cookiesPerHr: [],
 };
 
-var capHill = {
+var CapHill = {
   store: 'Capitol Hill',
   minCust: 20,
   maxCust: 38,
   avgCust: 2.3,
-  randNum: 0,
+  randNum: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + 1;
+  },
   cookiesPerHr: [],
 };
  
-var alki = {
+var Alki = {
   store: 'Alki',
   minCust: 2,
   maxCust: 16,
   avgCust: 4.6,
-  randNum: 0,
+  randNum: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + 1;
+  },
   cookiesPerHr: [],
 };
