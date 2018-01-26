d9af63f987ff8d502a3b8e29b4d7729c8dd919ee
David Johnson <djohnso08@yahoo.com>
2018-01-26 12:17:30 -0800

    added third and fourth store to sales js and html file

--- js/app.js
+++ js/app.js
@@ -83,3 +83,85 @@ var SeaTac = {
   }
 };
 SeaTac.render();
+
+var SeattleCenter = {
+  name: 'Seattle Center',
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  randCust: [],
+  cookiesPerHr: [],
+  totalCookies: 0,
+  calcRand: function() {
+    for (var i = 0; i < hoursArray.length; i++) {
+      this.randCust.push(
+        Math.floor(
+          Math.random() * (this.maxCust - this.minCust + 1)
+        ) + this.minCust
+      );
+    }
+  },
+  calcCookies: function() {
+    for (var j = 0; j < hoursArray.length; j++) {
+      this.cookiesPerHr.push(
+        Math.round(this.avgCookies * this.randCust[j])
+      );
+    }
+  },
+  render: function() {
+    var seattlecenter = document.getElementById('seattlecenter');
+    this.calcRand();
+    this.calcCookies();
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.name;
+    seattlecenter.appendChild(h3El);
+    for (var k = 0; k < hoursArray.length; k++) {
+      var liEl = document.createElement('li');
+      liEl.textContent =
+        hoursArray[k] + ': ' + this.cookiesPerHr[k] + ' cookies';
+      seattlecenter.appendChild(liEl);
+    }
+  }
+};
+SeattleCenter.render();
+
+var CapitolHill = {
+  name: 'Capitol Hill',
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  randCust: [],
+  cookiesPerHr: [],
+  totalCookies: 0,
+  calcRand: function() {
+    for (var i = 0; i < hoursArray.length; i++) {
+      this.randCust.push(
+        Math.floor(
+          Math.random() * (this.maxCust - this.minCust + 1)
+        ) + this.minCust
+      );
+    }
+  },
+  calcCookies: function() {
+    for (var j = 0; j < hoursArray.length; j++) {
+      this.cookiesPerHr.push(
+        Math.round(this.avgCookies * this.randCust[j])
+      );
+    }
+  },
+  render: function() {
+    var capitolhill = document.getElementById('capitolhill');
+    this.calcRand();
+    this.calcCookies();
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.name;
+    capitolhill.appendChild(h3El);
+    for (var k = 0; k < hoursArray.length; k++) {
+      var liEl = document.createElement('li');
+      liEl.textContent =
+        hoursArray[k] + ': ' + this.cookiesPerHr[k] + ' cookies';
+      capitolhill.appendChild(liEl);
+    }
+  }
+};
+CapitolHill.render();
