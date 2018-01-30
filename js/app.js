8860b466b659482a3df09471915669183e37af09
David Johnson <djohnso08@yahoo.com>
2018-01-29 19:06:11 -0800

    creating save point from saturday

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
-'use strict';
-var stores = [];
-var hoursArray = [' 6am ',' 7am ',' 8am ',' 9am ',' 10am ',' 11am ',' 12pm ',' 1pm ',' 2pm ',' 3pm ',' 4pm ',' 5pm ',' 6pm ',' 7pm ',' 8pm '];
-var locationTotals = 0;
+// 'use strict';
+// var stores = [];
 
-function Store(name, minCust, maxCust, avgCookies) {
+'use strict';
+//constructors! think of it like a specialized machine in a factory that creates new objects
+//prototypes are the functionality that the machine adds to the objects
+
+//we need to define our hours and store them
+var hoursArray = [' 6am ',' 7am ',' 8am ',' 9am ',' 10am ',' 11am ',' 12pm ',' 1pm ',' 2pm ',' 3pm ',' 4pm ',' 5pm ',' 6pm ',' 7pm ',' 8pm '];
+var storeTotals = 0;
+
+//we need to create a place for our list of all locations to be placed whenwe create them
+var locations = [];
+
+//we need to define a variable that will be dynamic depending on how many cookies
+var totalCookiesByHour = 0;
+//totalTotal
+var Total = 0;
+
+//constructor function begin with an Uppercase letter
+
+function Stores(name,minCust,maxCust,avgCookies) {
+  //a function called Stores will be a template for creating new objects that represent other locations
+  //the statements in this function add properties and or methods to the objects
+  //the this keyword is used instead of the object name to indicate that th property or method belongs to the object that this function creates
+  //this function has four parameters and each one sets the value of a property in the object
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookies = avgCookies;
+  //why is this an array?
   this.randCust = [];
   this.cookiesPerHr = [];
   this.totalCookies = 0;
-  stores.push(this);
+  locations.push(this);
+  //using the this keyword in front of the method to show that the method belongs to the object that this function creates
+  // this.calcRandCustByHour = function() {
+  // METHODS GO HERE
+  //THINK ABOUT PROTOTYPES
+  // }
   this.calcRand = function() {
     for (var i = 0; i < hoursArray.length; i++) {
       this.randCust.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
@@ -22,50 +48,48 @@ function Store(name, minCust, maxCust, avgCookies) {
       this.cookiesPerHr.push(Math.round(this.avgCookies * this.randCust[j]));
     }
   };
-  this.render = function() {
-    var table = document.getElementById('sales-section');
-    this.calcRand();
-    this.calcCookies();
-    var thEl = document.createElement('th');
-    for(var i = 0; i < stores.length; i++) {
-      var trEl = document.createElement('tr');
-      trEl.textContent = this.name;
-      thEl.appendChild(trEl);
-    }
-    table.appendChild(trEl);
-    console.log(table);
-  };
+  this.calcRand();
+  this.calcCookies();
 }
+//remember to call the METHODS in the constructor that are now prototypes available to the new objects the constructor will MakeLocation
 
+//call the functions in a function if you want
+//this constructor function creates five new objects with their own unique values used in properties of this object (name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour)
+function makeNewStore() {
+    new Stores('1st and Pike', 23, 65, 6.3);
+    new Stores('SeaTac Airport', 3, 24, 1.2);
+    new Stores('Seattle Center', 11, 38, 3.7);
+    new Stores('Capitol Hill', 20, 38, 2.3);
+    new Stores('Alki', 2, 16, 4.6);
+  //make one for each store
+}
+makeNewStore();
+
+//time to create the table in javascript
+//make header row
+//table needs an id in html
 
 function makeHeader() {
   var sales = document.getElementById('sales-section');
-
   var trEl = document.createElement('tr');
-  for(var i = 0;i < hoursArray.length;i++) {
-    var thEl = document.createElement('th');
+  var thEl = document.createElement('th');
+  trEl.appendChild(thEl);
+  for(var k = 0;k < hoursArray.length;k++) {
+    var header = document.createElemenet('th');
     thEl.textContent = hoursArray[i];
-    trEl.appendChild(thEl);
+    thEl.appendChild(header);
   }
-  sales.appendChild(trEl);
-  console.log(trEl);
+  makeHeader();
 }
 
-makeHeader();
+console.log(maNewStore());
 
 
+  //consider a for loop for the hours
+//create a th element
+//give the th element some text content like hours[i];
+//appendChild
 
-function newStores() {
-  new Store('1st and Pike', 23, 65, 6.3);
-  new Store('SeaTac Airport', 3, 24, 1.2);
-  new Store('Seattle Center', 11, 38, 3.7);
-  new Store('Capitol Hill', 20, 38, 2.3);
-  new Store('Alki', 2, 16, 4.6);
-}
-
-newStores();
-console.log(stores);
-
-for(var i = 0;i < stores.length;i++) {
-  stores[i].render();
-}
+//create a th element
+//give the th element text content 'Daily Location Total'
+//append child
